4c9a8d019f739f54b7b8de3b0d22a825c2581f4e
oi404118 <tiago.gouvea@oi.net.br>
2018-03-15 12:08:25 -0300

    Estruturação do projeto

---
+++ src/main/java/com/tiagogouvea/api/SpringbootAngular5BackendApplication.java
@@ -0,0 +1,12 @@
+package com.tiagogouvea.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngular5BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngular5BackendApplication.class, args);
+	}
+}
