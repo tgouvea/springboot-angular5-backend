22e4500b8d29698f3ee16f13a157dd8fa5b94e7a
oi404118 <tiago.gouvea@oi.net.br>
2018-03-20 19:07:27 -0300

    usuario controller

--- src/main/java/com/tiagogouvea/api/controller/UsuarioController.java
+++ src/main/java/com/tiagogouvea/api/controller/UsuarioController.java
@@ -4,8 +4,12 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,7 @@ import com.tiagogouvea.api.services.UsuarioService;
 
 @RestController
 @RequestMapping("/api/usuarios")
+@CrossOrigin(origins="*")
 public class UsuarioController {
 	
 	@Autowired
@@ -25,6 +30,39 @@ public class UsuarioController {
 	
 	@PostMapping
 	public ResponseEntity<Response<Usuario>> cadastrar(@Valid @RequestBody UsuarioDto usuarioDto, BindingResult result){
+		
+		Response<Usuario> response = new Response<>();
+		
+		validarSenhas(usuarioDto, result);
+		
+		if (result.hasErrors()) {
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		Usuario usuario = usuarioService.save(usuarioDto);
+		
+		if (usuario == null) {
+			response.getErrors().add("Email de usuário já cadastrado.");
+			return ResponseEntity.badRequest().body(response);
+		}else {
+			response.setData(usuario);
+			return ResponseEntity.ok(response);
+		}
+		
+	}
+
+
+	private void validarSenhas(UsuarioDto usuarioDto, BindingResult result) {
+		if (!usuarioDto.getSenha().equals(usuarioDto.getConfirmacaoDeSenha())) {
+			result.addError(new ObjectError("UsuarioDto", "As senhas não são iguais."));
+		}
+	}
+
+	
+	@PutMapping
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<Usuario>> atualizar(@Valid @RequestBody UsuarioDto usuarioDto, BindingResult result){
 		
 		Response<Usuario> response = new Response<>();
 		
@@ -48,10 +86,7 @@ public class UsuarioController {
 			return ResponseEntity.ok(response);
 		}
 		
-	}
-
-	
-	
+	}	
 
 
 }
